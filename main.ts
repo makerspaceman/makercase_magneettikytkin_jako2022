20d0b4b025d94d53bc730c32c1e24896ab310076
makerspaceman <91957914+makerspaceman@users.noreply.github.com>
2022-06-04 16:19:14 +0300

    Päivitys main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,11 +6,6 @@ basic.forever(function () {
         basic.pause(500)
         Kitronik_Robotics_Board.servoStop(Kitronik_Robotics_Board.Servos.Servo4)
     } else {
-        for (let index = 0; index < 5; index++) {
-            Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo4, 2)
-            basic.pause(400)
-            Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo4, 400)
-            basic.pause(500)
-        }
+        Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo4, 16)
     }
 })
