0e268e19c33cded2b7d44a81d9c126350662e516
makerspaceman <91957914+makerspaceman@users.noreply.github.com>
2022-06-04 16:57:13 +0300

    Päivitys main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,12 +2,12 @@ let Magneetti = 0
 basic.forever(function () {
     Magneetti = input.magneticForce(Dimension.Strength)
     if (Magneetti < 600) {
-        Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo4, 120)
-        basic.pause(500)
+        Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo4, 180)
+        basic.pause(1000)
         Kitronik_Robotics_Board.servoStop(Kitronik_Robotics_Board.Servos.Servo4)
-    } else if (Magneetti >= 600) {
-        Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo4, 16)
-        basic.pause(500)
+    } else {
+        Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo4, 0)
+        basic.pause(1000)
         Kitronik_Robotics_Board.servoStop(Kitronik_Robotics_Board.Servos.Servo4)
     }
 })
